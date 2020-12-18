aa1ff45f0eb1a48ee83cf45498b99c96a9e17355
abhisek <abhiseksahoo33@gmail.com>
2020-12-18 19:33:15 +0530

    added deafult employee responce

--- src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.example.springbootwebapi.controller;
 
+import com.example.springbootwebapi.model1.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class EmployeeController {
@@ -10,5 +13,12 @@ public class EmployeeController {
     public  String saysomthing()
     {
         return "I m learning spring-web-api and you ?";
+
+    }
+    @GetMapping("/employee/defaultemployee")
+    public Employee  defaultemployeedata()
+    {
+        Employee  e =new Employee(01, UUID.randomUUID().toString(),"Abhisek","Sahoo");
+        return e;
     }
 }
