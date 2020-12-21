e105ee0e1d0cb96923399c5fa9f907e1c0962045
abhisek <abhiseksahoo33@gmail.com>
2020-12-21 21:08:05 +0530

    calling employee details by selectbyuid

--- src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.example.springbootwebapi.controller;
 
 import com.example.springbootwebapi.model1.Employee;
+import com.example.springbootwebapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 public class EmployeeController {
+@Autowired
+    private EmployeeRepository EmployeeRepository;
 
     @GetMapping("/employee/hello")
     public  String saysomthing()
@@ -20,5 +25,10 @@ public class EmployeeController {
     {
         Employee  e =new Employee(01, UUID.randomUUID().toString(),"Abhisek","Sahoo");
         return e;
+    }
+    @GetMapping("/employee/{uuid}")
+    public Employee getemployeebyuuid(@PathVariable("uuid") String uid)
+    {
+    return EmployeeRepository.selectByUid(uid);
     }
 }
