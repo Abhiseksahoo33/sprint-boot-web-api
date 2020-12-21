393d362b0f779346dec9013b62735cb8adb70132
abhisek <abhiseksahoo33@gmail.com>
2020-12-21 21:13:22 +0530

    calling all employee details by selectall()

--- src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,9 +27,14 @@ public class EmployeeController {
         Employee  e =new Employee(01, UUID.randomUUID().toString(),"Abhisek","Sahoo");
         return e;
     }
-    @GetMapping("/employee/{uuid}")
+    @GetMapping("/employee/{uuid}") // uid is the resourse identifier
     public Employee getemployeebyuuid(@PathVariable("uuid") String uid)
     {
     return EmployeeRepository.selectByUid(uid);
+    }
+    @GetMapping("/employee/")
+    public List<Employee> getallemployeedetails()
+    {
+return  EmployeeRepository.selectAll();
     }
 }
